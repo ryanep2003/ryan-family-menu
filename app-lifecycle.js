bf006e1d75a3459d4ce788c1f1083a332c74c310
ryanep2003 <ryanep2003@gmail.com>
2026-09-10 21:04:13 -0400

    Restore service worker listener in reel prototype

--- app-lifecycle.js
+++ app-lifecycle.js
@@ -93,7 +93,7 @@ export function registerServiceWorker({ $, onUpdateAvailable }) {
     });
   }
 
-  navigatorObject?.serviceWorker?.addEventListener?.("controllerchange", () => {
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
     if (hadController) onUpdateAvailable();
     hadController = true;
   });
